296107515a9023a14de285253affeb85cbfa8236
Angel <villaangel305@gmail.com>
2025-09-23 23:02:27 -0600

    Cambio de tema, prueba de boton en js

---
+++ scriptlogin.js
@@ -0,0 +1,11 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+const boton = document.getElementById("btnIniciarSesion");
+
+boton.addEventListener("click", function(){
+
+    alert("Inicio de sesion confirmado")
+});
+
+});
+
